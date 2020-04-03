e406432e7933d37ef1a7cb8e404eb5956bf87095
ConOLeary <conolaoghaire@gmail.com>
2020-04-03 19:46:15 +0100

    bootstrapping implemented

--- main.py
+++ main.py
@@ -1,49 +1,57 @@
 import pygal
 import sys
+from random import randint
+from sort import sort
+import statistics
 LINE_ONE_ELEMS= 17
 MAKE_PERCENT= 100
-BOOTSTRAP_SAMPLE = 50
+BOOTSTRAP_SAMPLE_SIZE= 20
+BOOTSTRAP_AMOUNT_SAMPLES= 50 # return this to 10,000
+CONFIDNC_INTRVL_1= 15
+CONFIDNC_INTRVL_2= 95
+A_LARGE_NUMBER= 99999999
 
 f = open("midterm2020.txt", "r")
 data= f.read().split()
 
 ################################### (1) i) ###################################
-# get longest user 0 wait time, and total values for user0
-user0valCount= 0
-longestWait= 0
-temp= 0
-elems= len(data)
+# extract user 0 data from data
+user0data= []
 i= LINE_ONE_ELEMS
+elems= len(data)
 while(i < elems):
-    temp= int(data[i])
+    user0data.append(int(data[i]))
+    i+= 5
+
+# get longest user 0 wait time
+temp= 0
+longestWait= 0
+for i in range(len(user0data)):
+    temp= user0data[i]
     if(temp > longestWait):
-        longestWait= int(data[i])
-    i+=5
-    user0valCount+= 1
+        longestWait= user0data[i]
+        # print(longestWait)
 
 # initialise wait times frequency array, and prob of time array
 timesWaitedFreq= []
 probOfTime= []
-for i in range(longestWait + 1):
+for i in range(len(user0data)):
     timesWaitedFreq.append(0)
     probOfTime.append(0)
 
 # populate wait times frequency array
 i= LINE_ONE_ELEMS
-while(i < elems):
-    temp= int(data[i])
+for i in range(len(user0data)):
+    temp= user0data[i]
     timesWaitedFreq[temp]+= 1
-    i+=5
 
 # calculate probabilities from freqs
-i= 0
-while(i < longestWait + 1):
-    probOfTime[i]= (timesWaitedFreq[i] / user0valCount) * MAKE_PERCENT
-    i+= 1
+for i in range(len(user0data)):
+    probOfTime[i]= (timesWaitedFreq[i] / len(user0data)) * MAKE_PERCENT
 
 # load bar set for histo
-histoBarSet= [(0,0,0)] * (longestWait + 1)
-for i in range(longestWait + 1):
+histoBarSet= [(0,0,0)] * (len(user0data))
+for i in range(len(user0data)):
     histoBarSet[i]= (probOfTime[i], i, i+1)
 
 # make histo
@@ -52,14 +60,74 @@ pmf.add('', histoBarSet)
 pmf.render_to_file('q1.svg')
 
 ################################### (1) ii) ###################################
-zeros= 0
+
 ones= 0
-for i in range(longestWait + 1):
-    if(timesWaitedFreq[i] > 10):
-        ones+= 1
-    else:
-        zeros+= 1
-probOfOne= (ones / zeros) * MAKE_PERCENT
+allConnections= 0
+for i in range(len(timesWaitedFreq)):
+    if(i <= 10):
+        ones+= timesWaitedFreq[i]
+    allConnections+= timesWaitedFreq[i]
+probOfOne= (ones / allConnections) * MAKE_PERCENT
 print("Probability(X = 1): ", probOfOne)
 
 ################################### (1) iii) ###################################
+# BOOTSTRAPPING
+def getValsInIntrvl(vals, medianPos, intrvl):
+    valsIn= [vals[medianPos]]
+    i= medianPos # i is left pointer array offset
+    j= medianPos # j is right pointer array offset
+    intrvlSize= round((len(vals) / 100) * intrvl) - 1
+    while(len(valsIn) < intrvlSize):
+        #print("length: ",len(valsIn))
+        #print("                                (i,j): ",i,",",j)
+        if(i > 0):
+            leftValCost= vals[i] - vals[i - 1]
+            #print("leftValCost: ",leftValCost)
+        else:
+            leftValCost= A_LARGE_NUMBER
+        if(j < (len(vals) - 1)):
+            rightValCost= vals[j + 1] - vals[j]
+            #print("rightValCost: ",rightValCost)
+        else:
+            rightValCost= A_LARGE_NUMBER
+        if(leftValCost > rightValCost): # expand span to the right
+            j+= 1
+            valsIn.append(vals[j])
+            #print("value added: ", vals[j])
+        else: # expand span to the left
+            i-= 1
+            valsIn.append(vals[i])
+            #print("value added: ", vals[i])
+    return valsIn
+
+sampleMeans= []
+for i in range(BOOTSTRAP_AMOUNT_SAMPLES):
+    sampleSum= 0
+    for j in range(BOOTSTRAP_SAMPLE_SIZE):
+        random= randint(0, len(user0data) - 1)
+        sampleSum+= user0data[random]
+        # print(random)
+    sampleMeans.append(sampleSum / BOOTSTRAP_SAMPLE_SIZE)
+
+sampleMeans= sort(sampleMeans)
+
+for i in range(len(sampleMeans)):
+    print(sampleMeans[i])
+
+medianOfMeans= statistics.median(sampleMeans)
+i= 0
+while(medianOfMeans > sampleMeans[i]):
+    i+= 1
+medianOfMeans= sampleMeans[i]
+print("medianOfMeans: ",medianOfMeans)
+nintyFiveIntrvl= getValsInIntrvl(sampleMeans, i, CONFIDNC_INTRVL_2)
+print("------------------------------")
+for val in nintyFiveIntrvl:
+    print(val)
+fifteenIntrvl= getValsInIntrvl(sampleMeans, i, CONFIDNC_INTRVL_1)
+print("------------------------------")
+for val in fifteenIntrvl:
+    print(val)
+
+
+
---
+++ sort.py
@@ -0,0 +1,20 @@
+# A quicksort implementation stolen from StackOverflow:
+# https://stackoverflow.com/questions/18262306/quicksort-with-python
+def sort(array):
+    less = []
+    equal = []
+    greater = []
+    if len(array) > 1:
+        pivot = array[0]
+        for x in array:
+            if x < pivot:
+                less.append(x)
+            elif x == pivot:
+                equal.append(x)
+            elif x > pivot:
+                greater.append(x)
+        # Don't forget to return something!
+        return sort(less)+equal+sort(greater)  # Just use the + operator to join lists
+    # Note that you want equal ^^^^^ not pivot
+    else:  # You need to handle the part at the end of the recursion - when you only have one element in your array, just return the array.
+        return array
